c76ba14824accf15d586931370173a7b9aedd049
鄭妤婕 <b09902074@csie.ntu.edu.tw>
2023-11-30 13:17:41 +0000

    Update Application.java

--- backend/src/main/java/JobHunting/Application.java
+++ backend/src/main/java/JobHunting/Application.java
@@ -4,14 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-// import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 // import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-// import org.springframework.web.servlet.config.annotation.CorsRegistry;
-// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableAutoConfiguration(exclude={SecurityAutoConfiguration.class})
 
@@ -30,17 +30,17 @@ public class Application {
 		return new ResponseEntity<>(responseBody, HttpStatus.OK);
 	}
 
-	// @Bean
-	// public WebMvcConfigurer corsConfigurer() {
-	// 	return new WebMvcConfigurer() {
-	// 		@Override
-	// 		public void addCorsMappings(CorsRegistry registry) {
-	// 		registry.addMapping("/**")
-	// 			.allowedOrigins("*")
-	// 			.allowedMethods("*")
-	// 			.allowCredentials(false).maxAge(3600);
-	// 		}
-	// 	};
-	// }
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+			registry.addMapping("/**")
+				.allowedOrigins("*")
+				.allowedMethods("*")
+				.allowCredentials(false).maxAge(3600);
+			}
+		};
+	}
 
 }
