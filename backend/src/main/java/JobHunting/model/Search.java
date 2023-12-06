6396cef84f5881e4f65fb5f66fded101bfc4c1be
a113062130630210 <tienhsinglin.a@gmail.com>
2023-12-06 21:56:41 +0800

    can't find it in db

---
+++ backend/src/main/java/JobHunting/model/Search.java
@@ -0,0 +1,25 @@
+package JobHunting.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "search")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Search {
+  @Id private ObjectId _id;
+  private String company;
+  private String jobTitle;
+  private String level;
+
+  public void setJob(String company, String jobTitle, String level) {
+    this.company = company;
+    this.jobTitle = jobTitle;
+    this.level = level;
+  }
+}
