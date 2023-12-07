30658a4b0a949179058e486a68e6f966bd3cd5fd
a113062130630210 <tienhsinglin.a@gmail.com>
2023-12-08 05:45:43 +0800

    change to correct collections

--- backend/src/main/java/JobHunting/model/Search.java
+++ backend/src/main/java/JobHunting/model/Search.java
@@ -7,7 +7,7 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "search")
+@Document(collection = "jobs")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
