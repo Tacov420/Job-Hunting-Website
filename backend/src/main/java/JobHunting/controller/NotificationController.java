dae1659c7e8f08660062553a381f0e8caaf0e156
Jin <ngjinping@gmail.com>
2023-12-09 00:47:34 +0800

    test

--- backend/src/main/java/JobHunting/controller/NotificationController.java
+++ backend/src/main/java/JobHunting/controller/NotificationController.java
@@ -8,13 +8,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
 
 import java.util.*;
 
-// import javax.validation.Valid;
-
 import JobHunting.service.*;
 import JobHunting.model.*;
+
 import org.bson.types.ObjectId;
 
 @RestController
@@ -26,28 +26,46 @@ public class NotificationController {
     private NotificationService notificationService;
 
     @GetMapping("/{userName}")
-    public ResponseEntity<List<Notification>> getNotificationsByUser(@PathVariable String userName) {
+    public ResponseEntity<?> getNotificationsByUser(@PathVariable String userName) {
         try {
             List<Notification> notifications = notificationService.getNotificationsByUserName(userName);
 
-            // Iterate over notifications and mark each as read
-            notifications.forEach(notification -> {
-                notificationService.markAsRead(notification.getId()); // Assuming getId() exists in Notification
-            });
+            if (notifications.isEmpty()) {
+                // User or notifications not found
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No notifications found for user: " + userName);
+            }
 
+            notifications.forEach(notification -> {
+                notificationService.markAsRead(notification.getId()); // Mark as read
+            });
             return ResponseEntity.ok(notifications);
         } catch (Exception e) {
-            return ResponseEntity.internalServerError().build();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Internal Server Error: " + e.getMessage());
         }
     }
 
     @DeleteMapping("/{userName}/{notificationId}")
-    public ResponseEntity<Void> deleteNotification(@PathVariable ObjectId notificationId) {
+    public ResponseEntity<String> deleteNotification(@PathVariable String userName,
+            @PathVariable ObjectId notificationId) {
         try {
             notificationService.deleteNotification(notificationId);
-            return ResponseEntity.ok().build();
+
+            return ResponseEntity.ok("Notification deleted successfully.");
         } catch (Exception e) {
-            return ResponseEntity.internalServerError().build();
+            return ResponseEntity.internalServerError().body("Internal Server Error: " + e.getMessage());
         }
     }
+
+    @GetMapping("/test")
+    public ResponseEntity<String> test() {
+        try {
+            notificationService.checkAndNotifyNewJobs();
+
+            return ResponseEntity.ok("Test successful.");
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body("Internal Server Error: " + e.getMessage());
+        }
+    }
+
 }
